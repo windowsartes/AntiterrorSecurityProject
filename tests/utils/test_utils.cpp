825110e6dab29eef0528539f65964d79a54d0099
Nikolai Makarov <makarov.nikolai20@gmail.com>
2023-05-24 00:03:20 +0300

    test for input verification for getDifference added

--- tests/utils/test_utils.cpp
+++ tests/utils/test_utils.cpp
@@ -4,7 +4,37 @@
 #include <utils/utils.cpp>
 
 
-TEST(get_difference_3_channels, first_zeros) {
+TEST(get_difference, exception_equal_numner_of_channels) {
+    cv::Mat image1 = cv::Mat(10, 10, CV_8UC1, cv::Scalar::all(0));
+    cv::Mat image2 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(5));
+
+    EXPECT_THROW({
+        try {
+            getDifference(image1, image2);
+        } 
+        catch(const std::invalid_argument& exception) {
+            ASSERT_STREQ("First and second images must have the same number of channels;", exception.what());
+            throw;
+        }
+    }, std::invalid_argument);
+}
+
+TEST(get_difference, exception_1_or_3_channels) {
+    cv::Mat image1 = cv::Mat(10, 10, CV_8UC2, cv::Scalar::all(0));
+    cv::Mat image2 = cv::Mat(10, 10, CV_8UC2, cv::Scalar::all(5));
+
+    EXPECT_THROW({
+        try {
+            getDifference(image1, image2);
+        } 
+        catch(const std::invalid_argument& exception) {
+            ASSERT_STREQ("Images can have only 1 or 3 channels;", exception.what());
+            throw;
+        }
+    }, std::invalid_argument);
+}
+
+TEST(get_difference, 3_channels_first_zeros) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(0));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(5));
 
@@ -18,7 +48,7 @@ TEST(get_difference_3_channels, first_zeros) {
     }
 }
 
-TEST(get_difference_3_channels, second_zeros) {
+TEST(get_difference, 3_channels_second_zeros) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(5));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(0));
 
@@ -32,7 +62,7 @@ TEST(get_difference_3_channels, second_zeros) {
     }
 }
 
-TEST(get_difference_3_channels, max_value) {
+TEST(get_difference, 3_channels_max_value) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(255));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(0));
 
@@ -46,7 +76,7 @@ TEST(get_difference_3_channels, max_value) {
     }
 }
 
-TEST(get_difference_3_channels, min_value) {
+TEST(get_difference, 3_channels_min_value) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(0));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC3, cv::Scalar::all(255));
 
@@ -60,7 +90,7 @@ TEST(get_difference_3_channels, min_value) {
     }
 }
 
-TEST(get_difference_1_channels, first_zeros) {
+TEST(get_difference, 1_channel_first_zeros) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC1, cv::Scalar::all(0));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC1, cv::Scalar::all(15));
 
@@ -74,7 +104,7 @@ TEST(get_difference_1_channels, first_zeros) {
     }
 }
 
-TEST(get_difference_1_channels, second_zeros) {
+TEST(get_difference, 1_channel_second_zeros) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC1, cv::Scalar::all(15));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC1, cv::Scalar::all(0));
 
@@ -88,7 +118,7 @@ TEST(get_difference_1_channels, second_zeros) {
     }
 }
 
-TEST(get_difference_1_channel, max_value) {
+TEST(get_difference, 1_channel_max_value) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC1, cv::Scalar(255));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC1, cv::Scalar(0));
 
@@ -103,7 +133,7 @@ TEST(get_difference_1_channel, max_value) {
     }
 }
 
-TEST(get_difference_1_channel, min_value) {
+TEST(get_difference, 1_channel_min_value) {
     cv::Mat image1 = cv::Mat(10, 10, CV_8UC1, cv::Scalar(0));
     cv::Mat image2 = cv::Mat(10, 10, CV_8UC1, cv::Scalar(255));
 
